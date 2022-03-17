ac4da7594d6499e623c20a222c610658cbec1842
Juan <juan.gonzalez@salud.gob.sv>
2022-03-17 09:07:33 -0600

    Agregando Cupones

--- descuentos.js
+++ descuentos.js
@@ -14,6 +14,12 @@ console.log({
 });
 */
 
+const cupon = [
+    "CODE15",
+    "CODE25",
+    "CODE30"
+];
+
 function calcularPrecioConDescuento(precio,descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento)/ 100;
@@ -24,10 +30,29 @@ function calcularPrecioConDescuento(precio,descuento){
 function precioConDescuento(){
     const inputPrecio = document.getElementById("InputPrecio");
     const preciovalue = inputPrecio.value;
+/*
     const inputDescuento = document.getElementById("InputDescuento");
     const descuentovalue = inputDescuento.value;
+*/
+    const inputCupon = document.getElementById("InputCupon");
+    const cuponvalue = inputCupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(preciovalue, descuentovalue);
+    let descuento;
+
+    switch(cuponvalue) {
+      case cupon[0]: 
+        descuento = 15;
+      break;
+      case cupon[1]:
+        descuento = 25;
+      break;
+      case cupon[2]: 
+        descuento = 30;
+      break;
+    }
+
+    //const precioConDescuento = calcularPrecioConDescuento(preciovalue, descuentovalue);
+    const precioConDescuento = calcularPrecioConDescuento(preciovalue, descuento);
 
     const precioFinal = document.getElementById("precioFinal");
     precioFinal.innerText = "El precio con descuento es: $ " + precioConDescuento;
